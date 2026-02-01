1d2d7430047f8136fa4a22f0fda4aaff5b76eb25
Aivora1 <cosmic.official.yt.42@gmail.com>
2026-02-01 19:55:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -31,3 +31,9 @@ tapBtn.addEventListener('click', () => {
     tapBtn.disabled = true;
   }
 });
+
+Telegram.WebApp.sendData(JSON.stringify({
+  name: name,
+  birth_date: birth,
+  zodiac: zodiac
+}))
